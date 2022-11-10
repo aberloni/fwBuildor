c5e2dd466912545e5b3a2821733a08f6f424d5a6
ab <andre.berlemont@gmail.com>
2022-11-10 15:16:59 +0100

    fix errors

--- Editor/ProfilePlatforms/DataBuildSettingProfilScenes.cs
+++ Editor/ProfilePlatforms/DataBuildSettingProfilScenes.cs
@@ -17,79 +17,85 @@ using UnityEditor.SceneManagement;
 [CreateAssetMenu(menuName = "buildor/merger/create DataBuildSettingProfilScenes", order = 100)]
 public class DataBuildSettingProfilScenes : ScriptableObject
 {
-  public string filter = ""; // filter a string that the scene NEEDs to have
-  public string[] excludes; // filter things that are excluded
+    public string filter = ""; // filter a string that the scene NEEDs to have
+    public string[] excludes; // filter things that are excluded
 
-  public string[] paths; // can't use Scene type :/ (not serializable)
-  
+    public string[] paths; // can't use Scene type :/ (not serializable)
+
 #if UNITY_EDITOR
 
-  [ContextMenu("add")]
-  public void add()
-  {
-    //keep existing
-    List<EditorBuildSettingsScene> tmp = new List<EditorBuildSettingsScene>();
-    tmp.AddRange(EditorBuildSettings.scenes);
-    
-    for (int i = 0; i < paths.Length; i++)
+    [ContextMenu("add")]
+    public void add()
     {
-      bool found = false;
-      for (int j = 0; j < tmp.Count; j++)
-      {
-        if(tmp[j].path == paths[i])
-        {
-          found = true;
-        }
-      }
+        //keep existing
+        List<EditorBuildSettingsScene> tmp = new List<EditorBuildSettingsScene>();
+        tmp.AddRange(EditorBuildSettings.scenes);
 
-      if(!found)
-      {
-        tmp.Add(new EditorBuildSettingsScene(paths[i], true));
-      }
+        for (int i = 0; i < paths.Length; i++)
+        {
+            bool found = false;
+            for (int j = 0; j < tmp.Count; j++)
+            {
+                if (tmp[j].path == paths[i])
+                {
+                    found = true;
+                }
+            }
+
+            if (!found)
+            {
+                tmp.Add(new EditorBuildSettingsScene(paths[i], true));
+            }
+        }
+
+        EditorBuildSettings.scenes = tmp.ToArray();
     }
 
-    EditorBuildSettings.scenes = tmp.ToArray();
-  }
-
-  [ContextMenu("record")]
-  public void record()
-  {
-    List<string> tmp = new List<string>();
-
-    EditorBuildSettingsScene[] edScenes = EditorBuildSettings.scenes;
-    foreach(EditorBuildSettingsScene sc in edScenes)
+    [ContextMenu("record")]
+    public void record()
     {
-      bool add = true;
+        List<string> tmp = new List<string>();
 
-      if(filter.Length > 0)
-      {
-        if(!sc.path.Contains(filter))
+        EditorBuildSettingsScene[] edScenes = EditorBuildSettings.scenes;
+        foreach (EditorBuildSettingsScene sc in edScenes)
         {
-          add = false;
-        }
-      }
+            bool add = true;
 
-      if(add)
-      {
-        foreach (string exclude in excludes)
-        {
-          if (sc.path.Contains(exclude)) add = false;
+            if(filter != null)
+            {
+                if (filter.Length > 0)
+                {
+                    if (!sc.path.Contains(filter))
+                    {
+                        add = false;
+                    }
+                }
+            }
+            
+            if (add)
+            {
+                if(excludes != null)
+                {
+                    foreach (string exclude in excludes)
+                    {
+                        if (sc.path.Contains(exclude)) add = false;
+                    }
+                }
+            }
+
+            if (add)
+            {
+                Debug.Log(sc.path);
+                //tmp.Add(EditorSceneManager.GetSceneByPath(sc.path));
+                tmp.Add(sc.path);
+            }
         }
-      }
-      
-      if(add)
-      {
-        Debug.Log(sc.path);
-        //tmp.Add(EditorSceneManager.GetSceneByPath(sc.path));
-        tmp.Add(sc.path);
-      }
+
+        paths = tmp.ToArray();
+
+        EditorUtility.SetDirty(this);
     }
-
-    paths = tmp.ToArray();
-
-    EditorUtility.SetDirty(this);
-  }
 
 #endif
-  
+
 }
